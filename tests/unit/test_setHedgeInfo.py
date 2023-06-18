79eae91cf1581b2601b5b11b19446c4b7df9f5ad
Victor Chepkasov <92711919+SugaryVictor@users.noreply.github.com>
2023-06-18 14:21:57 +0500

    add tests

--- tests/unit/test_setHedgeInfo.py
+++ tests/unit/test_setHedgeInfo.py
@@ -1,13 +1,14 @@
 
 import pytest
-from brownie import accounts
-from scripts.deploy_hedging import deploy_contract
-from scripts.script import setHedgeInfo, getHedgeInfo
+from brownie import Hedging, accounts
+from scripts.deploy_hedging import deployContract
+from scripts.script import setHedgeInfo, getHedgeInfo, payPartyA, payPartyB, getContractBalance 
 
-def test_setHedgeInfo():
+def test_HedgeInfo():
     a = accounts[0]
     b = accounts[1]
-    deploy_contract(a)
+    deployContract(a)
+
     shelfLife = 1
     validHedgeInfo = (a.address, b.address, 0, 0, 190365,
                         86400 * shelfLife, 0, 0, 0, 
@@ -17,3 +18,40 @@ def test_setHedgeInfo():
     testHedgeInfo = getHedgeInfo()
 
     assert validHedgeInfo == testHedgeInfo
+
+def test_payA():
+    a = accounts[0]
+    b = accounts[1]
+    contract = deployContract(a)
+    deposit = 100
+    setHedgeInfo(b, 1, a)
+    print(f"Balance A: {getHedgeInfo()[2]}")
+
+    payPartyA(contract, a, f"{deposit} wei")
+
+    assert getHedgeInfo()[2] == deposit * 190365
+
+def test_payB():
+    a = accounts[0]
+    b = accounts[1]
+    contract = deployContract(a)
+    deposit = 100
+    setHedgeInfo(b, 1, a)
+    print(f"Balance B: {getHedgeInfo()[3]}")
+
+    payPartyB(contract, b, f"{deposit} wei")
+
+    assert getHedgeInfo()[3] == deposit * 190365
+
+def test_BalanceChanges():
+    a = accounts[0]
+    b = accounts[1]
+    contract = deployContract(a)
+    deposit = 100
+    setHedgeInfo(b, 1, a)
+    print(f"Balance: {getContractBalance(contract)}")
+
+    payPartyA(contract, a, f"{deposit} wei")
+    payPartyB(contract, b, f"{deposit} wei")
+
+    assert getContractBalance(contract) == 2 * deposit

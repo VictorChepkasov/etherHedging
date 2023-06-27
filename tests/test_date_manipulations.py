8ec23045ca41048a643648ac460dd09fb0ae5469
Victor Chepkasov <92711919+SugaryVictor@users.noreply.github.com>
2023-06-27 23:25:48 +0500

    fix errors in tests

--- tests/test_date_manipulations.py
+++ tests/test_date_manipulations.py
@@ -13,14 +13,14 @@ pytestmark = pytest.mark.parametrize(
     'shelfLife',
     [0, 7, 365, pytest.param(0.1, marks=pytest.mark.xfail)]
 )
-def test_HedgeInfo(hedge, shelfLife=7):
+def test_HedgeInfo(hedge, shelfLife):
     a, b, _ = hedge
     ethUSDData = getLatestETHUSDData()
     validHedgeInfo = [False, False, a.address, b.address, ethUSDData, 86400 * shelfLife, 0, 0, 0, False, False, False, False, 0, 0]
     setHedgeInfo(hedge[1], shelfLife, hedge[0])
     testHedgeInfo = getHedgeInfo()
     # print(f'VALID: {validHedgeInfo}')
-    # print(f'TEST: {testHedgeInfo}')
+    # print(f'TEST: {testHedgeInfo}')5hFmYt7h
     assert validHedgeInfo == testHedgeInfo
 
 def test_contractReactivate(hedge, shelfLife):
